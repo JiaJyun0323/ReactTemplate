b73e96052477559245646ea33d81f0ed55d7c971
JiaJyun0323 <shum5528@gmail.com>
2025-12-13 09:55:49 +0800

    note demo14

--- mod03_spa/src/components/Demo14_NoEffect2.jsx
+++ mod03_spa/src/components/Demo14_NoEffect2.jsx
@@ -4,7 +4,7 @@ import Demo14_Child from "./Demo14_Child.jsx";
 export default function Demo14_NoEffect2() {
   const [selectedUserId, setSelectedUserId] = useState("tony");
   const handleUserIdChange = (e) => setSelectedUserId(e.target.value);
-
+  //*note: use "key" let child component know it's a different param , so useState should be re-render
   return (
     <>
       User ID:
