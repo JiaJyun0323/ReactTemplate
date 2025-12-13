33677b4ce5f1e759155fd0b7a134cb31b208df58
JiaJyun0323 <shum5528@gmail.com>
2025-12-13 09:38:44 +0800

    note demo13

--- mod03_spa/src/components/Demo13_NoEffect1.jsx
+++ mod03_spa/src/components/Demo13_NoEffect1.jsx
@@ -3,12 +3,18 @@ import React, { useState, useEffect } from "react";
 export default function Demo13_NoEffect1() {
   const [price, setPrice] = useState(0);
   const [jpyRate, setJpyRate] = useState(0);
+  
+  //*NOTE: use Effect could re-render component
+  // but usestate also re-render when state change
+  // in this case: #1 will render 4 times, #2 render 2 times
 
+  // #1
   //   const [jpy, setjpy] = useState(0);
   //   useEffect(() => {
   //     setjpy((price / jpyRate).toFixed(1));
   //   }, [price, jpyRate]);
 
+  // #2
   const jpy = (price / jpyRate).toFixed(1);
 
   return (
